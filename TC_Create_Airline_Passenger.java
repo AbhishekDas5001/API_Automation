cd61360061e8d9f83ab35dfaf89245c18827ed67
AbhishekDas5001 <98099464+AbhishekDas5001@users.noreply.github.com>
2022-02-01 10:24:26 +0530

    Add files via upload

--- TC_Create_Airline_Passenger.java
+++ TC_Create_Airline_Passenger.java
@@ -1,165 +1,200 @@
 package test;
 
-import org.testng.annotations.Test;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.json.JSONObject;
+
 import org.testng.annotations.Test;
 import java.util.HashMap;
 import java.util.Map;
 import static io.restassured.RestAssured.given;
+/*import com.fasterxml.jackson.databind.ObjectMapper; 
+import com.fasterxml.jackson.annotation.JsonProperty;*/
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 
 public class TC_Create_Airline_Passenger {
+	String generatedId = RandomStringUtils.randomNumeric(12);
 	@Test
 	public void createAirline()
+
 	{
 		String url= "https://api.instantwebtools.net/v1/airlines";
-		
-		  String requestBody = "{\r\n" 
-		  + "    \"id\": \"90400501674\",\r\n" 
-		  + "    \"name\": \"Indian Airways\",\r\n" 
-		  + "    \"country\": \"India\",\r\n"
-	      + "    \"logo\": \"home/qq393/Downloads/Air-India-logo.jpg\",\r\n"
-		  + "    \"slogan\": \"From India\",\r\n" 
-		  + "    \"head_quaters\": \"Bangalore, India\",\r\n"  
-		  + "    \"website\": \"www.airindia.in\",\r\n"  
-		  + "    \"established\": \"1997\"\r\n" 
-		  + "}";
-		 
-		
+
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("id", generatedId);
+		requestBody.put("name", "Indian Airways");
+		requestBody.put("country","India" );
+		requestBody.put("logo", "home/qq393/Downloads/Air-India-logo.jpg");
+		requestBody.put("slogan", "From India");
+		requestBody.put("head_quaters", "Bangalore, India");
+		requestBody.put("website", "www.airindia.in");
+		requestBody.put("established", "1932");
+
 		Map<String, String> expectedHeaders = new HashMap<String, String>();
 		expectedHeaders.put("Content-Type", "application/json; charset=utf-8");
 		expectedHeaders.put("Server", "nginx/1.18.0");
 
 		given().log().all().header("Content-Type", "application/json; charset=utf-8")
-		.and().body(requestBody)
+		.and().body(requestBody.toString())
 		.when().post(url)
 		.then().log().all().statusCode(200)
 		.and().statusLine("HTTP/1.1 200 OK")
 		.and().headers(expectedHeaders);
 	}	
-	
-	
-	@Test(description = "Get Request for all user")
-	public static void GetAirline()
+
+
+
+	@Test(dependsOnMethods = {"createAirline"})
+	public void getAirline()
 	{
 
-		
-		  String url = "https://api.instantwebtools.net/v1/airlines/90400501673"; 
-		 
-		 
-		 
-		
+
+		String url = "https://api.instantwebtools.net/v1/airlines/" + generatedId; 
+
+
+
+
 		Map<String, String> expectedHeaders = new HashMap<String, String>();
 		expectedHeaders.put("Content-Type", "application/json; charset=utf-8");
 		expectedHeaders.put("Server", "nginx/1.18.0");
-		//expectedHeaders.put("Content-Encoding", "gzip");
 
 		given().log().all()
 		.when().get(url)
 		.then().log().all().assertThat().statusCode(200)
 		.and().statusLine("HTTP/1.1 200 OK")
 		.and().headers(expectedHeaders);
-		
+		System.out.println("abhishek" +generatedId);
+
 	}
-	
-	
-@Test
-	
+
+
+	@Test(dependsOnMethods = { "createAirline"} )
+
 	public void createPassenger()
-{
-	String url= "https://api.instantwebtools.net/v1/passenger";
-	
-	  String requestBody = "{\r\n" 
-	  + "    \"name\": \"Abhishek Das\",\r\n" 
-	  + "    \"trips\": \"500\",\r\n" 
-	  + "    \"airline\": \"90400501674\"\r\n"
-	  + "}";
-	 
-	
+	{
+		String url= "https://api.instantwebtools.net/v1/passenger";
+
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("name", "Abhishek Das");
+		requestBody.put("trips", "5004");
+		requestBody.put("airline",generatedId  );
+
+		Map<String, String> expectedHeaders = new HashMap<String, String>();
+		expectedHeaders.put("Content-Type", "application/json; charset=utf-8");
+		expectedHeaders.put("Server", "nginx/1.18.0");
+
+		given().log().all().header("Content-Type", "application/json")
+		.and().body(requestBody.toString())
+		.when().post(url)
+		.then().log().all().assertThat().statusCode(200)
+		.and().statusLine("HTTP/1.1 200 OK")
+		.and().headers(expectedHeaders);
+
 		
-		  Map<String, String> expectedHeaders = new HashMap<String, String>();
-		  expectedHeaders.put("Content-Type", "application/json; charset=utf-8");
-		  expectedHeaders.put("Server", "nginx/1.18.0");
-		 
-
-	given().log().all().header("Content-Type", "application/json")
-	.and().body(requestBody)
-	.when().post(url)
-	.then().log().all().assertThat().statusCode(200)
-	.and().statusLine("HTTP/1.1 200 OK")
-	.and().headers(expectedHeaders);
-}	
+	}	
 
 
-@Test
-public static void PatchPassenger()
-{
-	String url="https://api.instantwebtools.net/v1/passenger/61e907433ece1b5be356bf1f";
+
+	@Test(dependsOnMethods ={"createPassenger", "createAirline"} ) 
+	public void getUserDetails() { 
+		RequestSpecification requestSpec = new RequestSpecBuilder().build();
+		requestSpec.baseUri("https://instantwebtools.net");
+		requestSpec.basePath("fake/rest/api"); 
+		Response response = given().spec(requestSpec) .when() .get("/" + generatedId);
+		System.out.println("path value:" + response.path("data._id"));
+		
 	
-	String requestBody="{\r\n" 
-			  + "    \"name\": \"Akash \"\r\n" 
-			  + "}";
+		String id= String.valueOf(response.path("data._id"));
+		updatePassenger(id);
+		}
 
-	Map<String, String> expectedHeaders = new HashMap<String, String>();
-	expectedHeaders.put("Content-Type", "application/json; charset=utf-8");
-	expectedHeaders.put("Server", "nginx/1.18.0");
 
-given().log().all().header("Content-Type", "application/json")
-.and().body(requestBody)
-.when().patch(url)
-.then().log().all().assertThat().statusCode(200)
-.and().headers(expectedHeaders);
+
+	@Test(dependsOnMethods = {"createPassenger", "createAirline", "getUserDetails" })
+	public static void updatePassenger(String id)
+	{
+		
+
+
+		String url="https://api.instantwebtools.net/v1/passenger/" + id ;
+
+	
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("name", "ABHISHEK");
+
+		Map<String, String> expectedHeaders = new HashMap<String, String>();
+		expectedHeaders.put("Content-Type", "application/json; charset=utf-8");
+		expectedHeaders.put("Server", "nginx/1.18.0");
+
+
+		given().log().all().header("Content-Type", "application/json")
+		.and().body(requestBody.toString()) .when().patch(url)
+		.then().log().all().assertThat().statusCode(200)
+		.and().headers(expectedHeaders);
+
+
+	}
+
+
+	@Test(enabled = false)
+	public void modifyPassenger()
+	{
+		String url="https://api.instantwebtools.net/v1/passenger/61ea7402a69bfd4d44363b1f";
+
+		String requestBody="{\r\n"
+				+ "		\"name\":\"Abhilash\",\r\n"
+				+ "		\"trips\":\"888\",\r\n"
+				+ "		\"airline\":\"1\"\r\n"
+				+ "}";
+
+
+		Map<String, String> expectedHeaders = new HashMap<String, String>();
+		expectedHeaders.put("Content-Type", "application/json; charset=utf-8");
+		expectedHeaders.put("Server", "nginx/1.18.0");
+
+
+
+		given().log().all().header("Content-Type", "application/json")
+		.and().body(requestBody)
+		.when().put(url)
+		.then().log().all().assertThat().statusCode(200)
+		.and().headers(expectedHeaders);
+	}
+
+
+
+	@Test(enabled = false)
+	public void deletePassenger()
+	{
+		String url = "https://api.instantwebtools.net/v1/passenger/61e907433ece1b5be356bf1f"; 
+
+
+		Map<String, String> expectedHeaders = new HashMap<String, String>();
+		expectedHeaders.put("Content-Type", "application/json; charset=utf-8");
+		expectedHeaders.put("Server", "nginx/1.18.0");
+		//expectedHeaders.put("Content-Encoding", "gzip");
+
+		given().log().all()
+		.when().delete(url)
+		.then().log().all().assertThat().statusCode(200)
+		//.and().statusLine("HTTP/1.1 200 OK")
+		.and().headers(expectedHeaders);
+
+	}
 
 }
 
 
-@Test
-public void PutPassenger()
-{
-	String url="https://api.instantwebtools.net/v1/passenger/61ea7402a69bfd4d44363b1f";
-	
-	String requestBody="{\r\n"
-			+ "		\"name\":\"Abhilash\",\r\n"
-			+ "		\"trips\":\"888\",\r\n"
-			+ "		\"airline\":\"1\"\r\n"
-			+ "}";
-	
-	
-	  Map<String, String> expectedHeaders = new HashMap<String, String>();
-	  expectedHeaders.put("Content-Type", "application/json; charset=utf-8");
-	  expectedHeaders.put("Server", "nginx/1.18.0");
-	 
-	
-	
-	given().log().all().header("Content-Type", "application/json")
-	.and().body(requestBody)
-	.when().put(url)
-	.then().log().all().assertThat().statusCode(200)
-	.and().headers(expectedHeaders);
-}
 
 
 
-@Test
-public void DeletePassenger()
-{
-String url = "https://api.instantwebtools.net/v1/passenger/61e907433ece1b5be356bf1f"; 
-	 
-	 
-	 
-	
-	Map<String, String> expectedHeaders = new HashMap<String, String>();
-	expectedHeaders.put("Content-Type", "application/json; charset=utf-8");
-	expectedHeaders.put("Server", "nginx/1.18.0");
-	//expectedHeaders.put("Content-Encoding", "gzip");
 
-	given().log().all()
-	.when().delete(url)
-	.then().log().all().assertThat().statusCode(200)
-	//.and().statusLine("HTTP/1.1 200 OK")
-	.and().headers(expectedHeaders);
-	
-}
 
-}
+
+
+
+
 
 
